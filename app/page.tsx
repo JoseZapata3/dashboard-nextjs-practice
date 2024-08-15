cd5c45d520feadd8abf7221018bd216aead583c4
JoseZapata3 <josezapata75235@gmail.com>
2024-08-15 11:07:57 -0500

    fix: format

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import Image from 'next/Image'
+import Image from 'next/Image';
 import styles from '@/app/ui/home.module.css';
-import { lusitana } from '@/app/ui/fonts'
+import { lusitana } from '@/app/ui/fonts';
 
 export default function Page() {
   return (
